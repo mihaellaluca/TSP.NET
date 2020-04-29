fdea81522531de6c5c467eaac3c896f94a743c97
mihaellaluca <48389197+mihaellaluca@users.noreply.github.com>
2020-04-29 15:49:53 +0300

    grpc

---
+++ gRPC_si_EF/GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using gRPC_si_EF;
+using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			
+			using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+			var client = new Greeter.GreeterClient(channel);
+			var reply = await client.SayHelloAsync(
+			new HelloRequest { Name = "GreeterClient" });
+			Console.WriteLine("Greeting: " + reply.Message);
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
